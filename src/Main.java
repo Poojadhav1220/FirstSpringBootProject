aa3407928d4769ef4d18b4be2440e08267c277e3
Pooja Jadhav <pj7381775@gmail.com>
2023-03-04 08:54:25 +0530

    first comment successfully

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello pooja");
+        System.out.println("Hello ghati");
     }
+    //first comment for ghati
 }
